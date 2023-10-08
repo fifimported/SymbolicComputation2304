fa801d8a500338e9880ad7bedb864be8a351962f
Evgeny Leonov <Leonov.e@praguecityuniversity.cz>
2023-10-08 17:37:44 +0200

    Update interview_task_1_roman_yepanchenko.clj

--- clojure-course/src/clojure_course/week2/interview_task_1_roman_yepanchenko.clj
+++ clojure-course/src/clojure_course/week2/interview_task_1_roman_yepanchenko.clj
@@ -1,26 +1,36 @@
 (ns clojure-course.week2.interview-task-1-roman-yepanchenko)
 
 (def integersCounter {})
+(def integersIndexes {})
 
 (defn process
   [integers]
 
-  (doseq [integer integers]
+  (doseq [[index, integer] (map-indexed vector integers)]
 
     ; if integersCounter map has an integer as a key then increment it counter
-    ; else create a new key/value
+    ; else create a new key/value and fill integersIndexes map with a values index
     (if (contains? integersCounter integer)
-      (def integersCounter (assoc integersCounter integer (+ 1 (get integersCounter integer))))
-      (def integersCounter (assoc integersCounter integer 1)))
+      (do (def integersCounter (assoc integersCounter integer (+ 1 (get integersCounter integer)))))
+      (do
+        (def integersCounter (assoc integersCounter integer 1))
+        (def integersIndexes (assoc integersIndexes integer index)))
+      )
     )
 
-  ; creating new empty map and filling it with filtered value from integersCounter
-  (println (into {} (filter (fn [value]
-                              (= (get value 1) 1)
-                              ) integersCounter
-                            )
-                 )
-           )
+  ; print
+  (println
+    ; loop-and-replace-values map that contains filtered values
+    (map
+      ; every map value now is a {integer: index}
+      (fn [x] {x (get integersIndexes x)})
+      ; creating new empty vector and filling it with filtered keys (integers) from integersCounter
+      (into [] (keys (filter (fn [value]
+                               (= (get value 1) 1)
+                               ) integersCounter
+                             ))
+            ))
+    )
   )
 
 (process
